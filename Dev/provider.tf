3fbc3f697439abab92747485f42d5fb0ddf662cb
Jaydeepc1985 <jaydeep.chakraborty1985@gmail.com>
2025-06-05 13:24:16 +0530

    Update provider.tf

--- Dev/provider.tf
+++ Dev/provider.tf
@@ -9,7 +9,7 @@ terraform {
 # Terraform Provider Block
 provider "azurerm" {
   features {}
-  subscription_id = "845670d0-6329-46a7-97a7-fe36dbdce51b"
+  subscription_id = ""
 }
 # terraform {
 #   backend "azurerm" {
